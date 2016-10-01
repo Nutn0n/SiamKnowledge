23e4dd09d08aa58de339f7b961d74bff0ebcbe5b
innneang <innneang@Sarats-MacBook-Pro.local>
2016-10-01 23:16:59 +0700

    update users database migration

--- database/migrations/2014_07_02_230147_migration_cartalyst_sentinel.php
+++ database/migrations/2014_07_02_230147_migration_cartalyst_sentinel.php
@@ -97,8 +97,6 @@ class MigrationCartalystSentinel extends Migration
             $table->string('password');
             $table->text('permissions')->nullable();
             $table->timestamp('last_login')->nullable();
-            $table->string('first_name')->nullable();
-            $table->string('last_name')->nullable();
             $table->timestamps();
 
             $table->engine = 'InnoDB';
