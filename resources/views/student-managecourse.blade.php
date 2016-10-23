7ed40ffbfd1bad320f746eb00d952efc04fca77d
innneang <innneang@Sarats-MacBook-Pro.local>
2016-10-23 22:06:51 +0700

    git push

--- resources/views/student-managecourse.blade.php
+++ resources/views/student-managecourse.blade.php
@@ -21,7 +21,7 @@
 
       <!-- Start  loop -->
       @foreach($data['course']->interests as $interest)
-      @if($interest->user->id = $data['course']->tutor_id)
+      @if($interest->user->id == $data['course']->tutor_id)
 		<br>
       <div class="card-small card-request-small">
       <div style="background-image: url('@if($interest->user->profile->avatar!=NULL) {{Storage::url($interest->user->profile->avatar)}}@else https://api.adorable.io/avatars/100/{{$interest->user->profile->email}} @endif');background-size: cover;background-position: center;background-repeat: no-repeat;" class="teacher-pic"></div>
@@ -41,7 +41,7 @@
       <div style="background-image: url('@if($interest->user->profile->avatar!=NULL) {{Storage::url($interest->user->profile->avatar)}}@else https://api.adorable.io/avatars/100/{{$interest->user->profile->email}} @endif');background-size: cover;background-position: center;background-repeat: no-repeat;" class="teacher-pic"></div>
         <div class="card-detail">
           <div class="card-name teacher-name"><h1>{{$interest->user->profile->name}}</h1></div>
-          <div class="card-description">
+          <div class="card-description"> 
             <h2 class="card-time">พี่{{$interest->user->profile->calledname}}</h2><br>
             @if($data['available']==true)<a href="/viewmycourse/{{$data['course']->id}}/select/{{$interest->user_id}}" class="button button-orange">เลือกคนนี้</a>@endif
             <a href="{{route('tutorprofile', ['id'=>$data['course']->id,'tutorid'=>$interest->user_id])}}" class="button button-grey">ดูประวัติ</a>
