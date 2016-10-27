df33ba5ba212c404b50948808a7ec17cf7ebd10d
innneang <innneang@Sarats-MacBook-Pro.local>
2016-10-27 22:26:44 +0700

    bug fix

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -129,7 +129,7 @@ class JobController extends Controller
 		if($Course->verificationcode == $request->code){
 			$Tutor = User::find(Sentinel::getUser()->id);
 			$User = $Course->user;
-			$User->credit->reservedcredit -= $Course->credit;
+			$User->credit->reservedcredit = $User->credit->reservedcredit - $Course->credit;
 			$Tutor->credit->credit += $Course->credit;
 			$request->session()->flash('status', true);
 			$Course->verified = true;
