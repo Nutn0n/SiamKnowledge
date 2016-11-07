5af2e25e8848f677e0d677f72d529bed0dc84e29
innneang <innneang@Sarats-MacBook-Pro.local>
2016-11-08 00:54:26 +0700

    bug fix

--- resources/views/admin/search.blade.php
+++ resources/views/admin/search.blade.php
@@ -1,7 +1,7 @@
 <!--- start loop -->
     @foreach($results as $result)
             <div class="profile-item">
-              <span class="profile-picture" style=""></span>
+               <span class="profile-picture" style="background-image: url('@if($result->avatar!=NULL) {{Storage::url($result->avatar)}}@else https://api.adorable.io/avatars/100/{{$result->email}} @endif');background-size: cover;background-position: center;background-repeat: no-repeat;"></span>
               <span class="profile-text">
               <h2>{{$result->name}}</h2>
               <h3>{{$result->school}}</h3>
