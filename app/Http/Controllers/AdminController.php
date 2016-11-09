afb0eec5dab8718583a3a0a1a22985f0aa5af839
innneang <innneang@Sarats-MacBook-Pro.local>
2016-11-09 23:28:45 +0700

    bug fix

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -36,8 +36,8 @@ class AdminController extends Controller
     	return view('admin.search')->with('results', $result);
     }
     public function coursesearch($keyword){
-        $userid = Profile::where('name', 'LIKE', $keyword.'%')->get();
-        $course = Course::where('subject','LIKE', $keyword.'%')->get();
+        $userid = Profile::where('name', 'LIKE', '%'. $keyword.'%')->get();
+        $course = Course::where('subject',$keyword)->get();
         return view('admin.coursesearch')->with('data', ['profile'=>$userid, 'course'=>$course]);
     }
 }
